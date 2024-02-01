36079eefcee600f7e792f359424f2f4210b5082e
e0401368543 <e0401368543@uisrael.edu.ec>
2024-01-31 20:50:13 -0500

    api/tipoClientes

--- src/main/java/com/uisrael/microserviciocliente/controller/TipoClienteController.java
+++ src/main/java/com/uisrael/microserviciocliente/controller/TipoClienteController.java
@@ -3,18 +3,27 @@ package com.uisrael.microserviciocliente.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+import org.springframework.web.bind.annotation.RestController;
 
 import com.uisrael.microserviciocliente.model.TipoCliente;
-
+import com.uisrael.microserviciocliente.services.IClienteService;
 import com.uisrael.microserviciocliente.services.ITipoClienteService;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@RestController
+@RequestMapping("api/tipoClientes")
+@Getter
+@Setter
+@RefreshScope
 public class TipoClienteController {
 	
 	@Autowired
