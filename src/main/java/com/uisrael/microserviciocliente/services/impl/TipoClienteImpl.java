97f9153c31db8073882493ee1f2c0604f93f2543
e0401368543 <e0401368543@uisrael.edu.ec>
2024-01-31 20:47:19 -0500

    Metodos tipo cliente

--- src/main/java/com/uisrael/microserviciocliente/services/impl/TipoClienteImpl.java
+++ src/main/java/com/uisrael/microserviciocliente/services/impl/TipoClienteImpl.java
@@ -2,21 +2,30 @@ package com.uisrael.microserviciocliente.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.uisrael.microserviciocliente.model.TipoCliente;
+
+import com.uisrael.microserviciocliente.repository.ItipoClienteRepository;
 import com.uisrael.microserviciocliente.services.ITipoClienteService;
 
 public class TipoClienteImpl implements ITipoClienteService {
+	
+
+	@Autowired
+	private ItipoClienteRepository tipoCliente;
 
 	@Override
 	public boolean guardarTipoCliente(TipoCliente nuevo) {
 		// TODO Auto-generated method stub
-		return tipoClienteRepo.save(nuevo)!=null;
+		
+		return tipoCliente.save(nuevo)!=null;
 	}
 
 	@Override
 	public List<TipoCliente> listarTipoClientes() {
 		// TODO Auto-generated method stub
-		return null;
+		return tipoCliente.findAll();
 	}
 
 }
